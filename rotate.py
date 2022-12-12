17e7a3cb0b81c54ebe17f531caa4bd7a083064e2
Regis Oda <regis@otk.com.br>
2022-12-12 06:33:01 -0300

    gitignore

--- rotate.py
+++ rotate.py
@@ -5,7 +5,7 @@ from os.path import basename
 from scipy import ndimage
 
 
-def auto_rotate(img):
+def auto_rotate(img, draw_lines=False):
 
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_edges = cv2.Canny(img_gray, 100, 100, apertureSize=3)
@@ -14,14 +14,16 @@ def auto_rotate(img):
 
     angles = []
     for [[x1, y1, x2, y2]] in lines:
-        # DRAW LINES DETECTED
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+        if (draw_lines):
+            # DRAW LINES DETECTED
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
         angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
         angles.append(angle)
 
     median_angle = np.median(angles)
-    print(f"Angle is {median_angle:.04f}")
+    # print(f"Angle is {median_angle:.04f}")
     img_rotated = ndimage.rotate(img, median_angle)
 
     return (img_rotated, median_angle)
